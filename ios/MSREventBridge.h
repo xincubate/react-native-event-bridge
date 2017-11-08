63ac6163dbb0bd035e628ad13d58497020a23913
Mike Kruk <tamagokun@gmail.com>
2017-11-08 11:20:57 -0500

    Switch RN module import fallbacks (#18)
    See this issue for reference: https://github.com/facebook/react-native/issues/15775#issuecomment-327082159
    
    Seems like React doesn't like it when the fallback is the *new* import, the fallback should be the old way.

--- ios/MSREventBridge.h
+++ ios/MSREventBridge.h
@@ -5,13 +5,12 @@
 //  Copyright © 2017 mischneider. All rights reserved.
 //
 
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTEventEmitter.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#else // Compatibility for RN version < 0.40
+#import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
 #endif
 
 NS_ASSUME_NONNULL_BEGIN
